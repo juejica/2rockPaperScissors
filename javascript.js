920e1f7371045c5a0d86606630a8ed65aa49682f
JessicaP <111230954+juejica@users.noreply.github.com>
2025-02-19 20:07:51 +1300

    add playRound function

--- javascript.js
+++ javascript.js
@@ -1,29 +1,60 @@
-//computer choice
+//Logic to get the computer choice (without using an array)
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3)
 
   if (choice === 0) {
-    return 'Rock'
+    return 'rock'
   } else if (choice === 1) {
-    return 'Paper'
+    return 'paper'
   } else {
-    return 'Scissors'
+    return 'scissors'
   }
 }
 
-//human choice
-let input = prompt('Take your pick between Rock, Paper, or Scissors.')
+//Logic to get the human choice
+let input = prompt(
+  'Take your pick between Rock, Paper, or Scissors.'
+).toLowerCase()
 function getHumanChoice() {
-  let answer = input.toLowerCase()
-
-  if (answer === 'rock') {
+  if (input === 'rock') {
     return 'Rock'
-  } else if (answer === 'paper') {
+  } else if (input === 'paper') {
     return 'Paper'
-  } else if (answer === 'scissors') {
+  } else if (input === 'scissors') {
     return 'Scissors'
   } else {
     return 'Invalid choice. Please choose Rock, Paper, or Scissors.'
   }
 }
-console.log(getHumanChoice())
+
+//Logic to play a single round
+let humanScore = 0
+let computerScore = 0
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice = humanChoice.toLowerCase()
+  computerChoice = computerChoice.toLowerCase()
+
+  let capitaliseHumanChoice =
+    humanChoice[0].toUpperCase() + humanChoice.substring(1)
+  let capitaliseComputerChoice =
+    computerChoice[0].toUpperCase() + computerChoice.substring(1)
+
+  if (humanChoice === computerChoice) {
+    return "It's a tie!"
+  } else if (
+    (humanChoice === 'rock' && computerChoice === 'scissors') ||
+    (humanChoice === 'paper' && computerChoice === 'rock') ||
+    (humanChoice === 'scissors' && computerChoice === 'paper')
+  ) {
+    return `You win! ${capitaliseHumanChoice} beats ${capitaliseComputerChoice}.`
+  } else {
+    return `You lose! ${capitaliseHumanChoice} loses to ${capitaliseComputerChoice}.`
+  }
+}
+
+let humanChoice = getHumanChoice()
+let computerChoice = getComputerChoice()
+let result = playRound(humanChoice, computerChoice)
+
+console.log(result)
