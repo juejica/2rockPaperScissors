8a5a425d65b3522aadb15d4b300f66b8b16bce0d
JessicaP <111230954+juejica@users.noreply.github.com>
2025-02-19 15:56:57 +1300

    create logic for getHumanChoice

--- javascript.js
+++ javascript.js
@@ -1,12 +1,25 @@
-// computer choice
+//computer choice
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3)
 
   if (choice === 0) {
-    return 'rock'
+    return 'Rock'
   } else if (choice === 1) {
-    return 'paper'
+    return 'Paper'
   } else {
-    return 'scissors'
+    return 'Scissors'
   }
 }
+
+//human choice
+let input = prompt('Take your pick between Rock, Paper, Scissors.')
+function getHumanChoice() {
+  if (input.toLowerCase() === 'rock') {
+    return 'Rock'
+  } else if (input.toLowerCase() === 'paper') {
+    return 'Paper'
+  } else {
+    return 'Scissors'
+  }
+}
+console.log(getHumanChoice())
