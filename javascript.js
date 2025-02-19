3994b4f5c703f220471e658e533794dac0c17984
JessicaP <111230954+juejica@users.noreply.github.com>
2025-02-19 15:19:35 +1300

    Logic to get computerChoice
    randomly return one of the following string values: “rock”, “paper” or “scissors”.

--- javascript.js
+++ javascript.js
@@ -1,1 +1,11 @@
-console.log('Hellorh')
+// computer choice
+function getComputerChoice() {
+  let choice = Math.random() * 3
+  if (choice <= 1) {
+    return 'rock'
+  } else if (choice > 1 && choice <= 2) {
+    return 'paper'
+  } else {
+    return 'scissors'
+  }
+}
